2763a9bd7ee0aee63c854caf41d910da1607c8d7
HoangNam <huyquang0081@gmail.com>
2023-04-18 10:06:35 +0700

    staff ui

---
+++ App/Views/Pages/Staff/staff.js
@@ -0,0 +1,110 @@
+$(document).ready(function(){
+  $(".sidebar-btn").click(function(){
+      console.log("sdvnsdivsijvnsdfsjkjsnkdsnvkjdsnjvsd")
+      $(".wrapper").toggleClass("Mycollapse");
+  });
+});
+
+
+
+
+const calendarTable = document.querySelector('.calendar-table');
+const monthYear = document.querySelector('.month-year');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+let currentDate = new Date();
+let currentMonth = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+
+function buildCalendar() {
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+  const lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+  let day = 1;
+  let calendarHTML = '';
+
+  for (let i = 0; i < 6; i++) {
+    calendarHTML += '<tr>';
+
+    for (let j = 0; j < 7; j++) {
+      if (i === 0 && j < firstDayOfMonth) {
+        calendarHTML += '<td></td>';
+      } else if (day > lastDateOfMonth) {
+        calendarHTML += '<td></td>';
+      } else {
+        const date = new Date(currentYear, currentMonth, day);
+        const className = date.toDateString() === currentDate.toDateString() ? 'current-day' : '';
+        calendarHTML += `<td class="${className}" data-date="${date.toISOString()}">${day}</td>`;
+        day++;
+      }
+    }
+
+    calendarHTML += '</tr>';
+  }
+
+  const tbody = calendarTable.querySelector('tbody');
+  tbody.innerHTML = calendarHTML;
+
+  monthYear.textContent = `${getMonthName(currentMonth)} ${currentYear}`;
+}
+
+function getMonthName(monthIndex) {
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  return monthNames[monthIndex];
+}
+
+buildCalendar();
+
+prevButton.addEventListener('click', () => {
+  currentMonth--;
+  if (currentMonth < 0) {
+    currentMonth = 11;
+    currentYear--;
+  }
+  buildCalendar();
+});
+
+nextButton.addEventListener('click', () => {
+  currentMonth++;
+  if (currentMonth > 11) {
+    currentMonth = 0;
+    currentYear++;
+  }
+  buildCalendar();
+});
+
+
+function toggleCalendar() {
+  const calendarContainer = document.getElementById('calendar-container');
+  const body = document.getElementsByTagName('body')[0];
+  calendarContainer.style.display = calendarContainer.style.display === 'none' ? 'block' : 'none';
+  body.classList.toggle('show-calendar');
+}
+
+const cells = document.querySelectorAll('.calendar-table td');
+
+cells.forEach((cell) => {
+  cell.addEventListener('click', () => {
+    const selectedDate = new Date(cell.dataset.date);
+    const today = new Date();
+    const todayCell = document.querySelector('.calendar-table td.current-day');
+    if (todayCell) {
+      todayCell.classList.remove('current-day');
+    }
+    cell.classList.add('current-day');
+    const dateText = selectedDate.toDateString();
+    const todayText = today.toDateString();
+    const dateElement = document.querySelector('#day-select');
+    if (dateText === todayText) {
+      dateElement.textContent = 'Hôm nay';
+    } else {
+      dateElement.textContent = dateText;
+    }
+  });
+});
+
+
+
+  
+  
