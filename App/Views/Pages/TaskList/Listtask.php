5fa433c097cf94f3200d4c81c38b108a6b743add
dl123456123 <78434658+dl123456123@users.noreply.github.com>
2023-04-19 19:27:23 +0700

    task list

---
+++ App/Views/Pages/TaskList/Listtask.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>UWC</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="../../Component/style.css">
+        <script src="../../Component/app.js"></script>
+    </head>
+    <body>
+    <?php include '../../Component/component.php' ?>
+
+    <div class="wrapper">
+        <div class="main-container">
+            <!-- Code here -->
+        </div>
+    </div>
+
+    <script src="app.js"></script>
+    <script src="../../Component/app.js"></script>
+    </body>
+</html>
