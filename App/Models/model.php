f6fead280c2d588774666d3c15ca32e2ecbaa811
GutD0103 <ntdat0103@gmail.com>
2023-04-19 13:41:41 +0700

    add model

---
+++ App/Models/model.php
@@ -0,0 +1,58 @@
+<?php
+abstract class Employee{
+    public $employeeName;
+    public $employeeID;
+
+    public function __construct($employeeID, $employeeName){
+        $this->employeeID = $employeeID;
+        $this->employeeName = $employeeName;
+    }
+}
+
+class Janitor extends Employee{
+    public $employeeType = "janitor";
+}
+
+class Collector extends Employee{
+    public $employeeType = "collector";
+}
+
+class Collecting{
+    public $status;  // bool value, 1 is can use, 0 is can not
+    public $fuelConsumtion;
+    public $capacity;
+    public $weight;   // current weight of vihecle
+    public $collectingID;
+
+    public function __construct($status, $fuelConsumtion, $capacity, $collectingID){
+        $this->status = $status;
+        $this->fuelConsumtion = $fuelConsumtion;
+        $this->capacity = $capacity;
+        $this->collectingID = $collectingID;
+        $this->weight = 0;
+    }
+
+    public function __construct_2($fuelConsumtion, $capacity, $collectingID){
+        $this->status = false;
+        $this->fuelConsumtion = $fuelConsumtion;
+        $this->capacity = $capacity;
+        $this->collectingID = $collectingID;
+        $this->weight = 0;
+    }
+}
+
+class Troller{
+    public $status; // bool value, 1 is can use, 0 is can not
+    public $trollerID;
+
+    public function __construct($trollerID){
+        $this->trollerID = $trollerID;
+        $this->status = false;
+    }
+}
+
+class Task{
+    
+}
+
+?>
