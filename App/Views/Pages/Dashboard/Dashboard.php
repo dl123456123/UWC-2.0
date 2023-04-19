94f5b72173f9ae2dbbe3b894ad616e832dda7bdb
truongtran19315 <truong.chiptttran@gmail.com>
2023-04-19 08:19:17 +0700

    edit Dashboard

--- App/Views/Pages/Dashboard/Dashboard.php
+++ App/Views/Pages/Dashboard/Dashboard.php
@@ -12,7 +12,70 @@
 
     <div class="wrapper">
         <div class="main-container">
-             <!-- Code here -->
+            <!-- Code here -->
+            <div class="info-data">
+				<div class="card">
+					<div class="head">
+						<div>
+							<h2>1254</h2>
+							<p>Overload MCP</p>
+						</div>
+						<i class='bx bx-trending-up icon' ></i>
+					</div>
+					<span class="progress" data-value="40%"></span>
+					<span class="label">40%</span>
+				</div>
+				<div class="card">
+					<div class="head">
+						<div>
+							<h2>1254</h2>
+							<p>Empty MCP</p>
+						</div>
+						<i class='bx bx-trending-down icon down' ></i>
+					</div>
+					<span class="progress" data-value="60%"></span>
+					<span class="label">60%</span>
+				</div>
+				<div class="card">
+					<div class="head">
+						<div>
+							<h2>465</h2>
+							<p>Empty truck</p>
+						</div>
+						<i class='bx bx-trending-up icon' ></i>
+					</div>
+					<span class="progress" data-value="30%"></span>
+					<span class="label">30%</span>
+				</div>
+				<div class="card">
+					<div class="head">
+						<div>
+							<h2>235</h2>
+							<!-- <p>staff idle</p> -->
+                            <a href="staff_list.html">List staff </a>
+						</div>
+						<i class='bx bx-trending-up icon' ></i>
+					</div>
+					<span class="progress" data-value="80%"></span>
+					<span class="label">80%</span>
+				</div>
+			</div>
+
+            <div class="Charts">
+                <div style = "width: 50%;">
+                    <canvas id="column-chart"></canvas>
+                </div>
+
+                <div style="width: 50%;">
+                    <canvas id="line-chart"></canvas>
+                </div>
+            </div>
+
+            <div class="Map">
+                
+            </div>
+
+
         </div>
     </div>
 
